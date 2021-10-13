2b10b1c6cf05d39fd7b038b974bdd46fc5e5e40b
zhuangxiaozheng <1975489717@qq.com>
2021-10-13 16:07:27 +0800

    文件下载、解压以及路径优化

--- module_file/src/main/java/com/hongYi/h5container/business/FileOperateActivity.java
+++ module_file/src/main/java/com/hongYi/h5container/business/FileOperateActivity.java
@@ -21,11 +21,11 @@ import java.io.File;
  * Created by zhuangxiaozheng on 2021/9/30.
  */
 public class FileOperateActivity extends AppCompatActivity implements View.OnClickListener {
-    private Button download_file, unZipFolder_file, index_exist;
+    private Button download_file, index_exist;
 
     private String downloadUrl = "https://github.com/751496032/hYi-sdk/archive/refs/heads/main.zip";
-    private String h5FileNamePath = "hYi-jssdk-main/test.html";
-    File downloadFileName;
+    String fileName;
+    private String h5FileNamePath;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -36,13 +36,11 @@ public class FileOperateActivity extends AppCompatActivity implements View.OnCli
 
     private void initView() {
         download_file = findViewById(R.id.download_file);
-        unZipFolder_file = findViewById(R.id.unZipFolder_file);
         index_exist = findViewById(R.id.index_exist);
         index_exist.setOnClickListener(this);
         download_file.setOnClickListener(this);
-        unZipFolder_file.setOnClickListener(this);
-        downloadFileName = new File(downloadUrl);
-
+        fileName = Utils.getAppName(this);
+        h5FileNamePath = "file://" + FileUtil.getSDCardPath() + Utils.getAppName(this) + "/hYi-jssdk-main/test.html";
     }
 
 
@@ -50,13 +48,10 @@ public class FileOperateActivity extends AppCompatActivity implements View.OnCli
     public void onClick(View v) {
         int vid = v.getId();
         if (vid == R.id.download_file) {
-            DownLoaderTask downLoaderTask = new DownLoaderTask(downloadUrl, Utils.getAppName(v.getContext()), v.getContext());
+            DownLoaderTask downLoaderTask = new DownLoaderTask(downloadUrl, fileName, v.getContext());
             downLoaderTask.execute();
-        } else if (vid == R.id.unZipFolder_file) {
-            ZipExtractorTask zipExtractorTask = new ZipExtractorTask(Utils.getAppName(v.getContext()) + "/" + downloadFileName.getName(), Utils.getAppName(v.getContext()), v.getContext(), true);
-            zipExtractorTask.execute();
         } else if (vid == R.id.index_exist) {
-            startActivity(WebViewActivity.class, "file://" + FileUtil.getSDCardPath() + Utils.getAppName(v.getContext()) + "/" + h5FileNamePath, "测试网页");
+            startActivity(WebViewActivity.class, h5FileNamePath, "测试网页");
         }
     }
 
